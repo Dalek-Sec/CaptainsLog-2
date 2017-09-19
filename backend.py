8f862c343f59478f7ca593be4d8d60c4db138628
Dalek-Sec <sebastianxhanus@gmail.com>
2017-09-19 01:00:58 -0500

    Write code to add tags to junction table

--- backend.py
+++ backend.py
@@ -13,19 +13,24 @@ class Logbook:
         self.logs_to_tags = self.captainslogs_db["logs_to_tags"]
         
     def create_entry(self, text, tags, time=time.time()): # TODO: Implement proper use of transactions
+        # Create entry
         self.logs.insert(dict(text=text, time=time))
+        # Generate tags to entry
         self.check_tags(tags)
         entry_id = self.logs.find_one(text=text, time=time)["id"]
+        for tag in tags:
+            tag_id = self.tags.find_one(name=tag)["id"]
+            self.logs_to_tags.insert(dict(entry_id=entry_id, tag_id=tag_id))
 
     def check_tags(self, tags, create_if_missing=True):
-        all_tags_exist = True
+        all_tags_existed = True
         for tag in tags:
             if self.tags.find_one(name=tag) is None:
-                all_tags_exist = False
+                all_tags_existed = False
                 if create_if_missing is True:
                     self.tags.insert(dict(name=tag))
 
-        return all_tags_exist
+        return all_tags_existed
 
     def recent_entries(self, quantity):
         pass
