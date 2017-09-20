017cfbcf768d415321d93bba9d548403d7821eda
Dalek-Sec <sebastianxhanus@gmail.com>
2017-09-20 02:16:16 -0500

    Begin implementing screen to get session tags

--- frontend.py
+++ frontend.py
@@ -1,9 +1,12 @@
 def get_session_tags(lb):
     print(lb.get_recent_tag_sets(5))
+
+    tags = []
     while True:
-        pass
+        tag_input = input("Enter tag, or return empty to continue: ")
+        tags.append(tag_input)
     
-    return None
+    return tags
 
 def display_log_screen_prompt(lb, tags):
     pass
